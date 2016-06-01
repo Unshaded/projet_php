2c1a5105cc0a904828861a1df4b0b67f6a855acb
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-06-01 13:32:06 -0400

    class user

---
+++ user.php
@@ -0,0 +1,43 @@
+<?php
+
+class User{
+    private $id;
+    private $pseudo;
+    private $mdp;
+    private $email;
+
+    function __construct($pseudo,$mdp,$email){
+        $this->pseudo=($pseudo);
+        $this->mdp=($mdp);
+        $this->email=($email);
+    }
+
+    function getPseudo(){
+        return $this->pseudo;
+    }
+
+    function getId(){
+        return $this->id;
+    }
+
+    function getMdp(){
+        return $this->mdp;
+    }
+
+    function getEmail(){
+        return $this->email;
+    }
+
+    function setPseudo($new){
+        return $this->pseudo=($new);
+    }
+
+    function setMdp($new){
+        return $this->mdp=($new);
+    }
+
+    function setEmail($new){
+        return $this->email=($new);
+    }
+}
+?>
